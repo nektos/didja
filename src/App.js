d78c856794b3366bcea2414d7851da3d78d69793
Audrey Lee <audrey@nektos.com>
2020-03-15 22:13:32 +0000

    add icons and button to toggle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import hungry from './hungry.gif';
+import Fab from '@material-ui/core/Fab';
+import PetsIcon from '@material-ui/icons/Pets';
+import full from './full.gif';
 import './App.css';
 
+const period = 12 * 60 * 60 
+
 function App() {
+  const [lastTime, setLastTime] = useState(0);
+
+  function handleClick() {
+    setLastTime(Date.now());
+  }
+  
+  function isFed() {
+    let last = new Date(lastTime);
+    let now = new Date();
+    if(now - last > period) {
+      return false;
+    } else {
+      return true;
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {!isFed() && 
+        <React.Fragment>
+          <img src={hungry} alt="hungry"/>
+          Feed me, human!
+          <Fab color="primary" onClick={handleClick}> 
+            <PetsIcon />
+          </Fab>
+       </React.Fragment>
+      }
+      {isFed() &&
+       <React.Fragment>
+          <img src={full} alt="full"/>
+          I was fed. 
+       </React.Fragment>
+      }
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Last fed: {new Date(lastTime).toLocaleString()}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
+
   );
 }
 
